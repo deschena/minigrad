d77d619cc3c31cdb82b83ac7e1db2723f89806c4
Justin <justin.deschenaux@epfl.ch>
2021-04-14 10:04:53 +0200

    Implement loss and sequential

--- minigrad/nn.py
+++ minigrad/nn.py
@@ -1,40 +1,60 @@
 import torch
 from torch import empty
+from math import e
 
 class Module(object):
 
-    def forward(self, *input):
+    def __init__(self):
+        self.grad = None
+
+    def forward(self, input):
         raise NotImplementedError
 
-    def backward(self, *gradwrtoutput):
+    def backward(self, gradwrtoutput):
         raise NotImplementedError
+
+    def zero_grad(self):
+        self.grad = None
 
     def param(self):
         return []
 
 # ============================================================================================================
 
-class Linear(Module):
+class Loss(object):
 
+    def forward(self, pred, target):
+        raise NotImplementedError
+
+    def backward(self, pred, target):
+        raise NotImplementedError
+
+    def param(self):
+        return []
+# ============================================================================================================
+
+class Linear(Module):
     def __init__(self, input_features, output_features):
         super(Linear, self).__init__()
-        self.weights = torch.randn(size=(output_features, input_features), requires_grad=False)
+
+        # Xavier initialization
+        self.weights = torch.empty(size=(output_features, input_features), requires_grad=False).uniform_(- 1, 1) * (input_features) ** -0.5
         self.bias = torch.zeros(output_features, requires_grad=False)
         self.grad = None
 
     def forward(self, x):
-        return self.weights@x + self.bias 
+        return self.weights @ x + self.bias 
 
     def backward(self, gradwrtoutput, x):
-        dl_dw = gradwrtoutput.view(-1, 1)@x.view(1, -1)
+        dl_dw = gradwrtoutput.view(-1, 1) @ x.view(1, -1)
         dl_db = gradwrtoutput
         if self.grad is None:
-            self.grad = (dl_dw, dl_db)
+            self.grad = [dl_dw, dl_db]
         else:
             # accumulate gradient
             old_dw, old_db = self.grad
             self.grad = (old_dw + dl_dw, old_db + dl_db)
-        # Return gradient wrt to input, used by parent layer
+        # Return gradient wrt to input, used by parent layer (before activation)
         return self.weights.T @ gradwrtoutput
 
     def params(self):
@@ -47,10 +67,31 @@ class Sequential(Module):
     def __init__(self, *args):
         super(Sequential, self).__init__()
         self.modules = args
-
+        
 
     def forward(self, x):
-        pass
+        y = x
+
+        for m in self.modules:
+            y = m.forward(y)
+        return y
+
+    def backward(self, gradwrtoutput, x):
+        forward_values = [x]
+        y = x
+        for m in self.modules:
+            y = m.forward(y)
+            forward_values.append(y)
+        back_grad = gradwrtoutput
+        
+        for mod, layer_input in zip(self.modules[::-1], forward_values[-2::-1]):
+            back_grad = mod.backward(back_grad, layer_input)
+
+        return back_grad
+    
+    def zero_grad(self):
+        for m in self.modules:
+            m.zero_grad()
 
 # ============================================================================================================
 
@@ -81,10 +122,9 @@ class Tanh(Module):
 
 # ============================================================================================================
 
-class MSELoss(Module):
+class MSELoss(Loss):
     def forward(self, pred, target):
         return ((pred - target) ** 2).sum() / (len(pred))
 
     def backward(self, pred, target):
         return (pred - target) / len(pred) * 2
-
