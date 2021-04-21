d5a04a1670201798f07c147d15f7d17a1a9fc8cd
Justin <justin.deschenaux@epfl.ch>
2021-04-21 14:02:18 +0200

    Write docstrings

--- minigrad/test.py
+++ minigrad/test.py
@@ -15,6 +15,15 @@ torch.set_grad_enabled(False)
 # =====================================================================================================================================================================
 
 def generate_circle_samples(nb_samples, seed=42):
+    """Generate data as asked in the project description.
+
+    Args:
+        nb_samples (int): How many samples are required
+        seed (int, optional): Seed of random generator, for reproducibility. Defaults to 42.
+
+    Returns:
+        (samples, labels): (2D points in space, labels associated)
+    """
     torch.random.manual_seed(seed)
     samples = torch.rand(size=(nb_samples, 2))
     center = torch.tensor([[0.5, 0.5]])
@@ -30,15 +39,36 @@ def generate_circle_samples(nb_samples, seed=42):
 # =====================================================================================================================================================================
 
 def f1_score (true_positive, positive, gt_positive):
-    if positive == 0 or gt_positive == 0 or true_positive == 0:
+    """Compute the F1-score
+
+    Args:
+        true_positive (Int): Number of true positives
+        positive (Int): Number of positively classified samples
+        gt_positive (Int): Number of effectively positive samples
+
+    Returns:
+        float: F1-score
+    """
+    if positive <= 0 or gt_positive <= 0 or true_positive <= 0:
         return 0
     precision = true_positive / positive
     recall = true_positive / gt_positive
 
-    return 2 / (1/precision + 1/recall)
+    return 2 / (1 / precision + 1 / recall)
 # =====================================================================================================================================================================
 
 def compute_statistics(loss_fct, model, x, y):
+    """Compute statistics of the model on labelled data. Expect binary classification: output will be clipped/rounded to {0, 1}
+
+    Args:
+        loss_fct (Callable): Loss function used during training
+        model (nn.Module): ML model under assessment
+        x (Iterable): Samples to use to evaluate the model
+        y (Iterable): Associated labels, will be compared with prediction of the model
+
+    Returns:
+        (float, float, float): (mean loss, accuracy, F1-score)
+    """
     
     loss = 0
     true_positive = 0
@@ -63,6 +93,21 @@ def compute_statistics(loss_fct, model, x, y):
 # =====================================================================================================================================================================
 
 def train_model(model, loss_fct, optimizer, x_train, y_train, x_test, y_test, epochs = 300):
+    """Training loop of the model
+
+    Args:
+        model (nn.Module): Model to train
+        loss_fct (Callable): Performance measure. Of the form (pred, target) -> loss
+        optimizer (Optim): Optimizer observing model, used to update weights of the model
+        x_train (Iterable): Training samples
+        y_train (Iterable): Training labels
+        x_test (Iterable): Test samples
+        y_test (Iterable): Test labels
+        epochs (int, optional): Number of epochs to train the model. Defaults to 300.
+
+    Returns:
+        (6*Iterable): (Training loss, Train accuracy, Train F1-score, Test loss, Test accuracy, Test F1-score)
+    """
     train_losses = []
     train_accuracy = []
     train_f1_score = []
@@ -96,6 +141,18 @@ def train_model(model, loss_fct, optimizer, x_train, y_train, x_test, y_test, ep
 # =====================================================================================================================================================================
 
 def plot(x, y_train, y_test, metric, model_struct, filename, xlog=False, ylog=False):
+    """Plot metric related to training of the model
+
+    Args:
+        x (Iterable): x axis data
+        y_train (Iterable): Metric on train set
+        y_test (Iterable): Metric on test set
+        metric (str): Name of the metric used. Displayed in title
+        model_struct (str): Description of the model structure. Displayed in title
+        filename (str): Name of file where to save the figure
+        xlog (bool, optional): Use logscale on x axis. Defaults to False.
+        ylog (bool, optional): Use logscale on y axis. Defaults to False.
+    """
     fig, ax = plt.subplots()
     ax.set_title(f"{metric} ({model_struct})")
     ax.set_xlabel("Epoch")
@@ -110,6 +167,12 @@ def plot(x, y_train, y_test, metric, model_struct, filename, xlog=False, ylog=Fa
 # =====================================================================================================================================================================
 
 def circle_experiment(nb_samples=1000, nb_epoch=300):
+    """Simple first experiment with circular boundary
+
+    Args:
+        nb_samples (int, optional): Number of samples for train/test. Defaults to 1000.
+        nb_epoch (int, optional): Number of training epochs. Defaults to 300.
+    """
     x_train, y_train = generate_circle_samples(nb_samples, seed=42)
     x_test, y_test = generate_circle_samples(nb_samples, seed=66)
 
