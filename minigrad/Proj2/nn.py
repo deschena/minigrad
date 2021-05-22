851ccc1e2c55a53e9a480615d642715828c7c4eb
Justin <justin.deschenaux@epfl.ch>
2021-05-22 11:27:56 +0200

    Add submission folder

---
+++ minigrad/Proj2/nn.py
@@ -0,0 +1,205 @@
+import torch
+from torch import empty
+from math import e
+
+class Module(object):
+    """Base class for composable elements of models
+    """
+    def __init__(self):
+        self.grad = None
+
+    def forward(self, input):
+        """Compute forward pass
+
+        Args:
+            input (torch.tensor): Input of the layer
+        """
+        raise NotImplementedError
+
+    def backward(self, gradwrtoutput, x):
+        """Compute backward pass
+
+        Args:
+            gradwrtoutput (torch.tensor): Tensor of gradients wrt to ouput of layer
+            x (torch.tensor): Input tensor used in forward pass (required to compute some gradients)
+        """
+        raise NotImplementedError
+
+    def zero_grad(self):
+        """Erases gradients previously kept in memory
+        """
+        self.grad = None
+
+    def __call__(self, input):
+        """Allows to call forward pass as in torch
+        """
+        return self.forward(input)
+
+    @property
+    def params(self):
+        """Trainable parameters of the mode
+
+        Returns:
+            List[torch.Tensor]: List of all parameters
+        """
+        return []
+
+    # Disable setter of params. We only want the model to define them, not the outside world.
+    @params.setter
+    def params(self, new_value):
+        pass
+
+
+# ============================================================================================================
+
+class Linear(Module):
+    def __init__(self, input_features, output_features):
+        """Linear layer. Implements W @ X + b with Xavier initialization of weight matrix W
+
+        Args:
+            input_features (int): Number of input features
+            output_features (int): Number of output features
+        """
+        super(Linear, self).__init__()
+
+        # Xavier initialization
+        self.weights = torch.empty(size=(output_features, input_features)).uniform_(- 1, 1) * (input_features) ** -0.5
+        self.bias = torch.empty(output_features)
+        self.bias[:] = 0 # Init bias tensor
+
+    def forward(self, x):
+        return self.weights @ x + self.bias 
+
+    def backward(self, gradwrtoutput, x):
+         # view calls are for broadcasting. Each entry (i, j) of dl_dw 
+         # depends on entry i of gradwrtoutput and j of x
+        dl_dw = gradwrtoutput.view(-1, 1) @ x.view(1, -1)
+        dl_db = gradwrtoutput
+        if self.grad is None:
+            self.grad = [dl_dw, dl_db]
+        else:
+            # accumulate gradient
+            self.grad[0] += dl_dw
+            self.grad[1] += dl_db 
+        # Return gradient wrt to input, used by parent layer (before activation)
+        return self.weights.T @ gradwrtoutput
+
+    @property
+    def params(self):
+        return [self.weights, self.bias]
+# ============================================================================================================
+
+class Sequential(Module):
+    """Allows stacking of simpler layers
+    """
+    def __init__(self, *args):
+        super().__init__()
+        self.modules = list(args)
+
+    def add_module(self, module):
+        self.modules.append(module)
+        
+    def forward(self, x):
+        y = x
+        for m in self.modules:
+            y = m.forward(y)
+        return y
+
+    def backward(self, gradwrtoutput, x):
+        forward_values = [x]
+        y = x
+        # First needs to recompute the forward values
+        for m in self.modules:
+            y = m.forward(y)
+            forward_values.append(y)
+        back_grad = gradwrtoutput
+        
+        for mod, layer_input in zip(self.modules[::-1], forward_values[-2::-1]):
+            back_grad = mod.backward(back_grad, layer_input)
+
+        return back_grad
+
+    @property
+    def grad(self):
+        _grad = []
+        for m in self.modules:
+            if m.grad != None:
+                _grad += m.grad
+        return _grad
+
+    @grad.setter
+    def grad(self, new_value):
+        pass
+    
+    @property
+    def params(self):
+        _params = []
+        for m in self.modules:
+            _params += m.params # If a layer contains no tunable parameter, m.params = []
+        return _params
+    def zero_grad(self):
+        for m in self.modules:
+            m.zero_grad()
+# ============================================================================================================
+
+class ReLU(Module):
+    """ReLU activation function
+    """
+    def forward(self, x):
+        res = empty(size=x.shape) # Makes a copy of the input to avoid modifying the input in place
+        res[x <= 0] = 0
+        res[x > 0] = x[x > 0]
+        return res
+
+    def backward(self, gradwrtoutput, x):
+        mask = empty(size=x.shape)
+        mask[x <= 0] = 0
+        mask[x > 0] = 1
+        return gradwrtoutput * mask
+# ============================================================================================================
+
+class Tanh(Module):
+    """Tanh activation function
+    """
+    def forward(self, x):
+        ex = e ** x
+        e_x = e ** (-x)
+        return (ex - e_x) / (ex + e_x)
+
+    def backward(self, gradwrtoutput, x):
+        return gradwrtoutput * (1 - self.forward(x) ** 2)
+# ============================================================================================================
+
+class Sigmoid(Module):
+    """Sigmoid activation function
+    NOTE: it is possible to implement it directly using a Tanh layer
+    """
+    def forward(self, x):
+        return 1 / (1 + e ** -x)
+
+    def backward(self, gradwrtoutput, x):
+        s = self.forward(x)
+        return gradwrtoutput * s * (1 - s)
+# ============================================================================================================
+
+class Loss(object):
+
+    def forward(self, pred, target):
+        raise NotImplementedError
+
+    def backward(self, pred, target):
+        raise NotImplementedError
+
+    def __call__(self, pred, target):
+        return self.forward(pred, target)
+        
+# ============================================================================================================
+
+class MSELoss(Loss):
+    """Standard MSE Loss
+    """
+    def forward(self, pred, target):
+        return ((pred - target) ** 2).sum() / (len(pred))
+
+    def backward(self, pred, target):
+        return (pred - target) / len(pred) * 2
---
+++ minigrad/Proj2/optim.py
@@ -0,0 +1,73 @@
+class Optimizer:
+    def __init__(self, model, **kwargs):
+        raise NotImplementedError
+    # Assumes instances of Optimizer have a model attribute representing the model to optimize
+    def zero_grad(self):
+        self.model.zero_grad()
+    
+    def step(self):
+        raise NotImplementedError
+# ============================================================================================================
+
+class SGD(Optimizer):
+    """SGD Optimizer
+    """
+    def __init__(self, model, lr=0.001, momentum=0.0):
+        """Create an optimizer that will update parameters using SGD algorithm
+
+        Args:
+            model (nn.Module): Model whose parameters must be learnt
+            lr (float, optional): Learning rate / Step size. Defaults to 0.001.
+            momentum (float, optional): Momentum of SGD optimizer. Defaults to 0.
+        """
+        self.model = model
+        self.model_params = model.params # Store them directly as Sequential generates a new array each time
+        self.lr = lr
+        self.momentum = momentum
+        self.momentum_ma = []
+        # We do that even if we have no momentum in order to have more generic code:
+        # Since we use a zip in step, if the list is empty, then no update occurs
+        for _ in model.params:
+            self.momentum_ma.append(0)
+
+    def step(self):
+        model_grad = self.model.grad
+        model_params = self.model_params
+        # Update parameters
+        for i, (g, p, v) in enumerate(zip(model_grad, model_params, self.momentum_ma)):
+            if self.momentum == 0.0: # Avoid unecessary computations
+                v_t = g
+            else:
+                v_t = self.momentum * v + (1 - self.momentum) * g
+                self.momentum_ma[i] = v_t
+            p -= self.lr * v_t
+# ============================================================================================================
+
+class Adam(Optimizer):
+    """Adam Optimizer
+    """
+    def __init__(self, model, lr=0.001, betas=(0.9, 0.999), eps=1e-08):
+        self.model = model
+        self.model_params = model.params # Store them directly as Sequential generates a new array each time
+        self.lr = lr
+        self.momentum_ma = [] # First order moving average
+        self.sec_ma = [] # Second order moving average
+        for _ in self.model.params:
+            self.momentum_ma.append(0)
+            self.sec_ma.append(0)
+        self.betas = betas
+        self.eps = eps
+
+    def step(self):
+        model_grad = self.model.grad
+        model_params = self.model_params
+        b_0, b_1 = self.betas
+        # Update the moving average and update params
+        for i, (g, p, m, v) in enumerate(zip(model_grad, model_params, self.momentum_ma, self.sec_ma)):
+            m_t = b_0 * m + (1 - b_0) * g
+            v_t = b_1 * v + (1 - b_1) * (g ** 2)
+            # Save updated mov. avg. params for next iter
+            self.momentum_ma[i] = m_t
+            self.sec_ma[i] = v_t
+            # Actual param update
+            p -= self.lr / ( v_t ** 0.5 + self.eps ) * m_t
